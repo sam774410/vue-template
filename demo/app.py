5ce72aff1d44f313524c5996e702092db3d4cc28
Ming-Sheng,Lyu <sam850118sam@gmail.com>
2021-05-30 23:01:39 +0800

    add flask

---
+++ demo/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    return render_template('index.htm', a='from flask')
+    #return "456"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ demo/static/js/main.js
@@ -0,0 +1,58 @@
+const { reactive, ref, watch, onMounted } = Vue;
+
+
+const App = {
+  setup() {
+    let msg = ref("HIII");
+    let testa = 'test from vue 123';
+    let mdate = ref("");
+    let records = ref([]);
+    
+
+    onMounted(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(function (response) {
+        // handle success
+        
+        records.value = response.data;
+        console.log(records.value)
+        // $('#table tbody tr').append('<td><a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a><a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete </a></td>');
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+    });
+    
+    let hi = () => {
+      swal("Good job!", "You clicked the button!", "success");
+    };
+
+    let update_row = (idx)=> {
+      // console.log(idx);
+      console.log(records.value[idx])
+     
+    }
+    
+    let review_date = () =>{
+      const fp = flatpickr("#myID", {
+        minDate: "today",
+      });
+      fp.open();
+    }
+
+    watch(mdate, (_new, _old) => {
+      console.log(_new, _old)
+    })
+
+    return { testa, msg, hi, review_date, mdate, records, update_row };
+
+
+  },
+};
+App.delimiters = ["${", "}$"];
+
+Vue.createApp(App).mount("#appp");
